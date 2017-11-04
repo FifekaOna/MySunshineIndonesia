a8efef2185c030c136b322fdce3d2e2463a1b4d0
Fifeka Onanda Wahid <tcs.fifekaonandaw@xl.co.id>
2017-11-04 13:34:35 +0700

    binding view from layout to java

--- app/src/main/java/com/example/a212651/mysunshineindonesia/MainActivity.java
+++ app/src/main/java/com/example/a212651/mysunshineindonesia/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.a212651.mysunshineindonesia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView weatherTitle;
+    TextView weatherDescription;
+    TextView weatherTemperature;
+    ImageView imgView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        weatherTitle = (TextView) findViewById(R.id.weather_day_title);
+        weatherDescription= (TextView) findViewById(R.id.weather_description);
+        weatherTemperature= (TextView) findViewById(R.id.weather_temperature);
+        imgView= (ImageView) findViewById(R.id.weather_img);
+
+        weatherTitle.setText("Hari Minggu");
+        weatherDescription.setText("Panas Banget");
+        weatherTemperature.setText("70 Derajat Celcius");
+        imgView.setImageResource(R.mipmap.ic_launcher_round);
+
+
     }
 }
